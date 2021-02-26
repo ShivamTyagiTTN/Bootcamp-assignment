dbe46adb2db89af6b63e37a0306b7e29bca2687d
ShivamTyagiTTN <78858737+ShivamTyagiTTN@users.noreply.github.com>
2021-02-26 19:12:10 +0530

    Improved naming convention

--- Multi-Threading/Q15.java
+++ Multi-Threading/Q15.java
@@ -7,41 +7,41 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Q15 {
 
-    Lock lock = new ReentrantLock(true);
-    Condition condition = lock.newCondition();
+    Lock myReentrantLock = new ReentrantLock(true);
+    Condition condition = myReentrantLock.newCondition();
 
     public void methodOne() {
         try {
-            lock.lock();
+            myReentrantLock.lock();
             System.out.println("Method 1 Started");
             condition.await();
             System.out.println("Method 1 Finished");
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
-            lock.unlock();
+            myReentrantLock.unlock();
         }
     }
 
     public void methodTwo(){
         try {
-            lock.lock();
+            myReentrantLock.lock();
             System.out.println("Method 3 Started");
             System.out.println("Method 3 Finished");
             condition.signal();
         }  finally {
-            lock.unlock();
+            myReentrantLock.unlock();
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
 
-        Q15 obj = new Q15();
+        Q15 q15obj = new Q15();
 
         System.out.println("Thread starting: " + Thread.currentThread().getName());
 
-        Thread thread1 = new Thread(obj::methodOne);
-        Thread thread2 = new Thread(obj::methodTwo);
+        Thread thread1 = new Thread(q15obj::methodOne);
+        Thread thread2 = new Thread(q15obj::methodTwo);
 
         thread1.start();
         thread2.start();
