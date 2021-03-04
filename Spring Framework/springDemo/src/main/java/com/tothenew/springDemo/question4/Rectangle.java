977dce2b9668b1f2ffecfd619e8fe599de3720a4
ShivamTyagiTTN <78858737+ShivamTyagiTTN@users.noreply.github.com>
2021-03-04 17:23:28 +0530

    Delete Rectangle.java

--- Spring Framework/springDemo/src/main/java/com/tothenew/springDemo/question4/Rectangle.java
+++
@@ -1,9 +0,0 @@
-package com.tothenew.springDemo.question4;
-
-public class Rectangle implements Drawable {
-    @Override
-    public void draw() {
-
-        System.out.println("Drawing Rectangle");
-    }
-}
