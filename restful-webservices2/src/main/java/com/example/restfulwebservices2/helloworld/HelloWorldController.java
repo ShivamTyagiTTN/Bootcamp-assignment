ace247acf8e6a10b2c692dcd7bfda28f751a2cf4
ShivamTyagiTTN <78858737+ShivamTyagiTTN@users.noreply.github.com>
2021-03-09 19:11:53 +0530

    Update HelloWorldController.java

--- restful-webservices2/src/main/java/com/example/restfulwebservices2/helloworld/HelloWorldController.java
+++ restful-webservices2/src/main/java/com/example/restfulwebservices2/helloworld/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.example.restfulwebservices2.helloworld;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -16,12 +17,14 @@ public class HelloWorldController {
 //  Q.1 Add support for Internationalization in your application allowing messages to be shown in
 //    English, German and Swedish, keeping English as default.
     @GetMapping("/morning")
+    @ApiOperation(value = "Morning wish", notes = "internationalized morning message")
     public String morningWish() {
         return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 
 //  Q.2 Create a GET request which takes "username" as param and shows a localized message "Hello Username".
     @GetMapping("/welcome")
+    @ApiOperation(value = "Welcome user", notes = "Internationalized welcome message ")
     public String welcomeUser(@RequestParam String userName) {
         String welcomeMessage=  messageSource.getMessage("welcome.message",
                 null, LocaleContextHolder.getLocale());
