aafa7012457887524b692623be5fd5e6e2c3e1a4
ShivamTyagiTTN <78858737+ShivamTyagiTTN@users.noreply.github.com>
2021-03-09 19:13:32 +0530

    Update EmployeeController.java

--- restful-webservices2/src/main/java/com/example/restfulwebservices2/filtering/EmployeeController.java
+++ restful-webservices2/src/main/java/com/example/restfulwebservices2/filtering/EmployeeController.java
@@ -3,6 +3,7 @@ package com.example.restfulwebservices2.filtering;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +15,14 @@ import java.util.List;
 public class EmployeeController {
 
     @GetMapping("/employee")
+    @ApiOperation(value = "Get employee", notes = "Get employees static filtering")
     public Employee getEmployee() {
         return new Employee("Vipin", "xyz@321");
     }
 
 //Q.9 Dynamic filtering
     @GetMapping("/employee-list")
+    @ApiOperation(value = "Employee list", notes = "Get employees using dynamic filtering")
     public MappingJacksonValue getAllEmployees() {
 
         List<Employee> employeeList = Arrays.asList(
